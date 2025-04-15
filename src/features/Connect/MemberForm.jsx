ace8d09307b73d1d2bcb2852a131ca554c76c139
Roy Mosby <ego.madking@gmail.com>
2025-04-15 15:19:18 -0400

    add rejoin checkbox

--- src/features/Connect/MemberForm.jsx
+++ src/features/Connect/MemberForm.jsx
@@ -3,6 +3,8 @@ import styles from './MemberForm.module.css';
 export default function MemberForm() {
   return (
     <form className={styles.joinForm}>
+      <label htmlFor="rejoin">Rejoin existing stack?</label>
+      <input type="checkbox" />
       <label htmlFor="stackToken">Stack Token</label>
       <input type="text" id="stackToken" />
       <label htmlFor="username">Display Name</label>
